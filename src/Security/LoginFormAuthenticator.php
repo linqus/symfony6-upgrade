ec8421489bee8861951b7a2d68e55d7cba8ad40f
Karol Linkiewicz <karol@linkiewicz.pl>
2023-03-10 20:25:14 +0100

    poprawka 2

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -19,7 +19,7 @@ use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
-
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 
 class LoginFormAuthenticator extends AbstractLoginFormAuthenticator
 {

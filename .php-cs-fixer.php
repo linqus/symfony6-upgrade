3c29922b9efb7e838029269315b9c8f79b3f91e0
Karol Linkiewicz <karol@linkiewicz.pl>
2023-03-10 13:00:35 +0100

    konfiguracja php-cs-fier

---
+++ .php-cs-fixer.php
@@ -0,0 +1,10 @@
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__.'/src')
+;
+$config = new PhpCsFixer\Config();
+return $config->setRules([
+        '@Symfony' => true,
+        'yoda_style' => false,
+    ])
+    ->setFinder($finder)
+;
